4308c07e54856eda7389596a365bca0e48fec3d4
Jude <judelaurentetejada@gmail.com>
2021-08-08 15:00:34 +0800

    lesson 32 - Set User query

---
+++ server/src/graphql/resolvers/User/index.ts
@@ -0,0 +1,10 @@
+import { IResolvers } from 'apollo-server-express';
+
+export const userResolvers: IResolvers = {
+
+  Query: {
+    user: () => {
+      return 'Query.user'
+    }
+  }
+};
--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -16,6 +16,7 @@ export const typeDefs = gql`
 
   type Query {
     authUrl: String!
+    user: String!
   }
 
   type Mutation {
