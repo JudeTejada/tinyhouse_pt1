39445192c7438a256fff11521a3e2b8401bf3a5d
Jude <judelaurentetejada@gmail.com>
2021-10-03 11:16:49 +0800

    add listings query

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -4,7 +4,14 @@ import { Request } from 'express';
 
 import { Listing, Database, User } from '../../../lib/types';
 import { authorize } from '../../../lib/util';
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from './types';
+import {
+  ListingArgs,
+  ListingBookingsArgs,
+  ListingBookingsData,
+  ListingsArgs,
+  ListingsData,
+  ListingsFilter
+} from './types';
 
 export const listingsResolvers: IResolvers = {
   Query: {
@@ -29,6 +36,38 @@ export const listingsResolvers: IResolvers = {
         return listing;
       } catch (error) {
         throw new Error(`Failed to query listing : ${error}`);
+      }
+    },
+    listings: async (
+      _root: undefined,
+      { filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: []
+        };
+
+        //find bookings that is eqaul to the id we passed
+        let cursor = await db.listings.find({});
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({ price: 1 });
+        }
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({ price: -1 });
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query  listings : ${error}`);
       }
     }
   },
